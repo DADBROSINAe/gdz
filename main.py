5cfc4dd0e1c384ea4bdadfb505ce11ff4c35f713
c2914 <day4492@gmail.com>
2022-12-17 11:04:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+a= int(input())
+if a == 1:
+    print ("січень")
+elif a == 2:
+    print ("лютий")
+elif a == 3:
+    print ("березень")
+elif a == 4:
+    print ("квітень")
+elif a == 5:
+    print("травень")
+elif a == 6:
+    print("червень")
+elif a == 7:
+    print("липень")
+elif a == 8:
+    print("сервень")
+elif a == 9:
+    print("вересень")
+elif a == 10:
+    print("жовтень")
+elif a == 11:
+    print("листопад")
+elif a == 12:
+    print("грудень")
